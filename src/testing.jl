25219b303d9e1a673c94863354448cf4a49b97df
LionelROLLAND <lionel.rolland@protonmail.com>
2024-02-06 14:22:38 +0100

    [+] Debut du framework de test.

---
+++ src/testing.jl
@@ -0,0 +1,50 @@
+using Graphs
+using MetaGraphsNext
+import JSON
+
+function testMethod(;
+    method::Function,
+    method_name::String,
+    save::String,
+    instance_dir::String,
+)::Nothing
+    save_file = ReentrantLock()
+    function instanceWrapper(filename::String)::Nothing
+        println("Processing $filename...")
+        graph::MetaGraph = open(
+            readInstance |> graphFromData,
+            joinpath("instance_dir", filename),
+        )
+        elapsed = @elapsed result = method(graph)
+        result_dict::Dict{String,Union{String,Int64,Float64}} = Dict{String,Union{String,Int64,Float64}}(
+            "file" => filename,
+            "method" => method_name,
+            "is_feasible" => Int64(result.is_feasible),
+            "proven_optimality" => Int64(result.proven_optimality),
+            "solving_time" => result.time,
+        )
+        while !trylock(save_file)
+        end
+        data = open(fd -> JSON.parse(fd), save, read=true)
+        push!(data, result_dict)
+        open(fd -> JSON.print(fd, data), save, write=true)
+        unlock(save_file)
+        return nothing
+    end
+
+    if !isfile(save)
+        open(
+            fd -> JSON.print(fd, []),
+            save,
+            write=true,
+            create=true,
+        )
+    end
+    save_file = ReentrantLock()
+    @sync begin
+        for file in readdir(instance_dir)
+            Threads.@spawn instanceWrapper(file)
+        end
+    end
+    return nothing
+end
